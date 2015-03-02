d480f295e7a8aac7cd5781200cc9f724b43d4c0d
Thomsen <changsen4584@163.com>
2015-03-02 17:12:52 +0800

    CommentBox component

--- www/js/app.js
+++ www/js/app.js
@@ -2,3 +2,18 @@ React.render(
   <h1>hello react</h1>,
   document.getElementById('example')
 );
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        I am a CommentBox.
+     </div>
+    );
+  }
+});
+
+React.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
